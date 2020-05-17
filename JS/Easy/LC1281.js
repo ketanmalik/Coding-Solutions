9a61871da39f720c863e3891cbcf14e7a797caa9
ketanmalik <malik.ke@husky.neu.edu>
2020-05-16 20:55:44 -0500

    lc1281

---
+++ JS/Easy/LC1281.js
@@ -0,0 +1,37 @@
+/*
+Leetcode #1281
+
+Given an integer number n, return the difference between the product of its digits and the sum of its digits. 
+
+Example 1:
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+
+Example 2:
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+
+@author ketanmalik
+May 16 2020
+*/
+
+var subtractProductAndSum = function (n) {
+  var p = 1;
+  var s = 0;
+  while (n > 0) {
+    p *= n % 10;
+    s += n % 10;
+    n = parseInt(n / 10, 10);
+  }
+  return p - s;
+};
+
+console.log(subtractProductAndSum(234));

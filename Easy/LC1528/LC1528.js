9593b0077cd3efc6e1f33e8cf3b63ad991f02875
ketanmalik <ketanmalik@gmail.com>
2021-05-18 01:16:38 -0400

    lc1528

--- Easy/LC1528/LC1528.js
+++ Easy/LC1528/LC1528.js
@@ -3,13 +3,21 @@
  * @param {number[]} indices
  * @return {string}
  */
-const restoreString = (s, indices) => {
-    const len = s.length;
-    if(len <= 1) return s;
-    let idx = 0;
-    let arr = [];
-    for(let c of s) {
-        arr[indices[idx++]] = c;
-    }
-    return arr.join('');
-};
+const restoreString = (s, indices) => (
+    indices.reduce((acc, curr, idx) => {
+        acc[curr] = s[idx];
+        return acc;
+    }, []).join("")
+);
+
+// Brute Force
+// const restoreString = (s, indices) => {
+//     const len = s.length;
+//     if(len <= 1) return s;
+//     let idx = 0;
+//     let arr = [];
+//     for(let c of s) {
+//         arr[indices[idx++]] = c;
+//     }
+//     return arr.join('');
+// };

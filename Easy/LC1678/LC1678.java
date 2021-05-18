1d08903ce4b524a9035fdfbc095e8c9945997b03
ketanmalik <ketanmalik@gmail.com>
2021-05-18 03:26:53 -0400

    lc1678

---
+++ Easy/LC1678/LC1678.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String interpret(String command) {
+        return command.replace("()", "o").replace("(al)", "al");
+    }
+}
+
+
+/* Brute Force Solution */
+// StringBuilder sb = new StringBuilder();
+// for(int i=0; i<command.length(); i++) {
+//     char c = command.charAt(i);
+//     if (c == 'G') {
+//         sb.append(c);
+//     } else if (c == '(') {
+//         if(command.charAt(i + 1) == ')') {
+//             sb.append("o");
+//             i++;
+//         } else {
+//             sb.append("al");
+//             i += 3;
+//         }
+//     }
+// }
+// return sb.toString();
